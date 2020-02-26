abf56fdba5b4cfeb9c89e7f16b526df83f1b73a2
YuMing Zhang <yumingzhang94@gmail.com>
2020-02-26 02:57:33 -0500

    reset working in summary

--- Finenance/Finenance/ViewControlls/Summary.swift
+++ Finenance/Finenance/ViewControlls/Summary.swift
@@ -9,8 +9,9 @@
 import SwiftUI
 
 struct Summary: View {
-    
+    @Environment(\.managedObjectContext) var moc
     @FetchRequest(entity: Couple.entity(), sortDescriptors: []) var couples : FetchedResults<Couple>
+    @FetchRequest(entity: SpentItems.entity(),sortDescriptors:[]) var items : FetchedResults<SpentItems>
     var body: some View {
         VStack
             {
@@ -22,11 +23,36 @@ struct Summary: View {
                         Text(cp.tab ?? "unknow")
                     }
                 }
-        }
+                
+                Button(action:{
+                    
+                    for item in self.items
+                    {
+                        self.moc.delete(item)
+                    }
+                    
+                    for couple in self.couples
+                    {
+                        couple.tab = ""
+                    }
+                    
+                    try? self.moc.save()
+                    
+                })
+                {
+                    Text("Reset")
+                }
+                
+                
+        }.navigationBarTitle(Text("Tab Summary"))
    
     }
     
 }
+
+
+
+
 
 struct Summary_Previews: PreviewProvider {
     static var previews: some View {
